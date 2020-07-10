6a3b95d5d8e208d8bc14736c2aa5faf350b13270
Akash Pradhan <ajax123crypto0@gmail.com>
2020-07-10 22:19:49 +0530

    Update Keywords for C11 and C++17
    The list of keywords for the latest C standard being C11 is here:
    https://en.cppreference.com/w/c/keyword
    
    The list of C++17 keywords are here:
    https://en.cppreference.com/w/cpp/keyword

--- kilo.cpp
+++ kilo.cpp
@@ -304,21 +304,25 @@ namespace kilopp
              std::vector<const char *>{
                  /* C Keywords */
                  "auto", "break", "case", "continue", "default", "do", "else", "enum",
-                 "extern", "for", "goto", "if", "register", "return", "sizeof", "static",
-                 "struct", "switch", "typedef", "union", "volatile", "while", "NULL",
+                 "extern", "for", "goto", "if", "register", "restrict", "return", "sizeof", 
+                 "static", "struct", "switch", "typedef", "union", "volatile", "while", 
+                 "NULL", "_Alignas", "_Alignof", "_Atomic", "_Bool", "_Complex", "_Generic",
+                 "_Imaginary", "_Noreturn", "_Static_assert", "_Thread_local",
 
                  /* C++ Keywords */
                  "alignas", "alignof", "and", "and_eq", "asm", "bitand", "bitor", "class",
-                 "compl", "constexpr", "const_cast", "deltype", "delete", "dynamic_cast",
+                 "compl", "constexpr", "const_cast", "decltype", "delete", "dynamic_cast",
                  "explicit", "export", "false", "friend", "inline", "mutable", "namespace",
                  "new", "noexcept", "not", "not_eq", "nullptr", "operator", "or", "or_eq",
                  "private", "protected", "public", "reinterpret_cast", "static_assert",
                  "static_cast", "template", "this", "thread_local", "throw", "true", "try",
-                 "typeid", "typename", "virtual", "xor", "xor_eq",
+                 "typeid", "typename", "using", "virtual", "xor", "xor_eq",
 
                  /* C types */
                  "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
-                 "void|", "short|", "auto|", "const|", "bool|"},
+                 "void|", "short|", "auto|", "const|", "bool|", "wchar_t|", "char8_t|", 
+                 "char16_t|", "char32_t|", 
+                 },
              "//",
              "/*",
              "*/",
